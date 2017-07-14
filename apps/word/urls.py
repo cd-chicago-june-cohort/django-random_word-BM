649646a6abcde9fbe178d88b7eb7a39d0e5f9891
Bald Mike <baldmike312@gmail.com>
2017-07-14 15:06:53 -0500

    initial commit

---
+++ apps/word/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from views import index, reset
+
+
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^reset', reset)
+]
+
+
---
+++ apps/word/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse, redirect
+from time import strftime, localtime
+from django.utils.crypto import get_random_string
+
+
+
+def index(request):
+    
+    if 'counter' not in request.session:
+        request.session['counter']=0
+
+    context = {
+        "word": get_random_string(length=12)
+    }
+
+    request.session['counter'] += 1
+
+    return render(request, 'word/index.html', context)
+    
+
+def reset(request):
+
+    request.session.clear()
+
+    return redirect('/')
